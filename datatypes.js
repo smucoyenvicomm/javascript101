9c61b7638f56043dec55ff4216b665c9672ea209
smucoyenvicomm <envicomm.mucoy@gmail.com>
2024-01-05 14:57:48 +0800

    message

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+let name = "Shahn Mucoy"
+console.log (name)
+let age = 22
+console.log (age)
+let isMarried = 21
+if (isMarried > age)
+{console.log("YES")
+
+}
+else {
+   console.log("NO")
+}
+
+let nameAndAge = "shahn" + 21
+console.log(nameAndAge)
+let sum = 2023 + 1
+console.log(sum)
+let product = 1012*2
+console.log(product)
+
+
+let helloWorld = "Hello World"
+console.log(helloWorld)
+let sum1 = 21 + 22
+console.log (sum1)
+let multiply = 21*22
+console.log(multiply)
+let greet = "Hello"
+console.log(greet,"Shahn") 
